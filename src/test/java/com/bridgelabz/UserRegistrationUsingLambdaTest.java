caea4670e60a9486e39b50d70e46c302da4dab48
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-21 15:09:43 +0530

    Added:Adding Numeric

--- src/test/java/com/bridgelabz/UserRegistrationUsingLambdaTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationUsingLambdaTest.java
@@ -44,6 +44,6 @@ public class UserRegistrationUsingLambdaTest {
     void GivenPasswordShouldWrittenTrue() {
         boolean password = userRegistration.ValidatePassword("Rajnishr");
         Assertions.assertTrue(password);
-        System.out.println("Password Checked(Rule 1 And 2 Checked)");
+        System.out.println("Password Checked(Rule 1 And 2 and 3 Checked)");
     }
 }
