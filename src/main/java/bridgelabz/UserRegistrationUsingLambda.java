e8c017c0053471ac189b9089589197ebb4e32a8f
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-21 15:06:42 +0530

    Added:Added UpperCase

--- src/main/java/bridgelabz/UserRegistrationUsingLambda.java
+++ src/main/java/bridgelabz/UserRegistrationUsingLambda.java
@@ -2,6 +2,7 @@ package bridgelabz;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+//uc1and2
 public class UserRegistrationUsingLambda {
     public boolean validate(String Firstname) {
         Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
@@ -21,7 +22,7 @@ public class UserRegistrationUsingLambda {
         else
             return false;
     }
-
+//uc3
     public boolean validateEmail(String Email) {
         Pattern pattern = Pattern.compile("^[0-9a-zA-Z]+([+.-]([a-z0-9A-Z]+))*[@][a-zA-Z0-9]+[.][a-z]{2,4}[,]?([.][a-z]{2,4})?$");
         Matcher matcher = pattern.matcher(Email);
@@ -30,7 +31,7 @@ public class UserRegistrationUsingLambda {
         else
             return false;
     }
-
+//uc4
     public boolean validatePhoneNumber(String Phone) {
         Pattern pattern = Pattern.compile("^[0-9]{2,}[0-9]{10,}$");
         Matcher matcher = pattern.matcher(Phone);
@@ -39,7 +40,7 @@ public class UserRegistrationUsingLambda {
         else
             return false;
     }
-
+//uc5
     public boolean ValidatePassword(String password) {
         Pattern pattern = Pattern.compile("^[A-Za-z]{8,}$");
         Matcher matcher = pattern.matcher(password);
